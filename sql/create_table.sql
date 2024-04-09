75e6db0a685d122f3ada91d0a68c3a28f19038e9
doxita <12522472+qiqiaoshuang@user.noreply.gitee.com>
2024-04-09 12:51:28 +0800

    项目初始化

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -3,10 +3,10 @@
 # @from <a href="https://yupi.icu">编程导航知识星球</a>
 
 -- 创建库
-create database if not exists my_db;
+create database if not exists yuoj;
 
 -- 切换库
-use my_db;
+use yuoj;
 
 -- 用户表
 create table if not exists user
@@ -41,6 +41,46 @@ create table if not exists post
     isDelete   tinyint  default 0                 not null comment '是否删除',
     index idx_userId (userId)
 ) comment '帖子' collate = utf8mb4_unicode_ci;
+
+-- 题目表
+create table if not exists question
+(
+    id          bigint auto_increment comment 'id' primary key,
+    title       varchar(512)                       null comment '标题',
+    content     text                               null comment '内容',
+    tags        varchar(1024)                      null comment '标签列表（json 数组）',
+    judgeConfig text                               null comment '判题配置,json对象',
+    judgeCase   text                               null comment '判题用例,json数组',
+    submitNum   int      default 0                 not null comment '题目提交数',
+    acceptedNum int      default 0                 not null comment '题目通过数',
+    answer      text                               null comment '题目答案',
+    thumbNum    int      default 0                 not null comment '点赞数',
+    favourNum   int      default 0                 not null comment '收藏数',
+    userId      bigint                             not null comment '创建用户 id',
+    createTime  datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime  datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete    tinyint  default 0                 not null comment '是否删除',
+    index idx_userId (userId)
+) comment '题目' collate = utf8mb4_unicode_ci;
+
+
+-- 帖子点赞表（硬删除）
+create table if not exists question_submit
+(
+    id         bigint auto_increment comment 'id' primary key,
+    language   varchar(128)                       not null comment '编程语言',
+    code       text                               not null comment '用户代码',
+    status     int      default 0                 not null comment '判题状态,(0 - 等待, 1 判题中, 2成功, 3 失败)',
+    judgeInfo  text                               not null comment '判题信息(json对象)',
+    questionId bigint                             not null comment '问题id',
+    userId     bigint                             not null comment '提交人 id',
+    createTime datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete   tinyint  default 0                 not null comment '是否删除',
+    index idx_postId (questionId),
+    index idx_userId (userId)
+) comment '题目提交表';
+
 
 -- 帖子点赞表（硬删除）
 create table if not exists post_thumb
